334937e7244e90e2377c285d539d433fc7800379
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-03-06 00:25:23 +0100

    fix: versioning

--- apps/ng-primitives-docs/src/main.ts
+++ apps/ng-primitives-docs/src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-fetch('/assets/config.json')
+fetch('./assets/config.json')
   .then((response) => response.json())
   .then((config) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
