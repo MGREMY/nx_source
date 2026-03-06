d7ab840b17b91b2beb0556cbcfd8430aebc2c58b
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-03-06 13:20:38 +0100

    docs(ng-primitives): update path location strategy

--- apps/ng-primitives-docs/src/app/core/config/app.config.ts
+++ apps/ng-primitives-docs/src/app/core/config/app.config.ts
@@ -3,15 +3,15 @@ import { APP_CONFIG_SERVICE } from '@mgremy/core';
 
 import {
   APP_BASE_HREF,
-  HashLocationStrategy,
   LocationStrategy,
+  PathLocationStrategy,
 } from '@angular/common';
 import { inject, Provider } from '@angular/core';
 
 export function provideApplicationConfig(): Provider[] {
   return [
     { provide: APP_CONFIG_SERVICE, useClass: AppConfigService },
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    { provide: LocationStrategy, useClass: PathLocationStrategy },
     {
       provide: APP_BASE_HREF,
       useFactory: () => inject(APP_CONFIG_SERVICE).appBaseHref,
