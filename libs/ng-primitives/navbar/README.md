0b4cfd580dd5a17075c14dd9e2fa75a7d455554e
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-03-07 19:02:16 +0100

    feat(ng-primitives): remove navbar

--- libs/ng-primitives/navbar/README.md
+++
@@ -1,4 +0,0 @@
-# @mgremy/ng-primitives/src/lib/navbar
-
-Secondary entry point of `@mgremy/ng-primitives`. It can be used by importing from
-`@mgremy/ng-primitives/navbar`.
--- libs/ng-primitives/navbar/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/mgnp-navbar/mgnp-navbar.directive';
-export * from './lib/mgnp-navbar-content/mgnp-navbar-content.directive';
-export * from './lib/mgnp-navbar-item/mgnp-navbar-item.directive';
