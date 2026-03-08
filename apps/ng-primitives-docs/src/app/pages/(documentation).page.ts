f529579e5302d5ea368e3ca2413401aa1e96a25d
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-03-08 17:54:57 +0100

    docs(ng-primitives): update not found and index pages

--- apps/ng-primitives-docs/src/app/pages/(documentation).page.ts
+++ apps/ng-primitives-docs/src/app/pages/(documentation).page.ts
@@ -1,20 +1,18 @@
 import { SidebarComponent } from '../components/sidebar/sidebar.component';
 import { APP_THEME_SERVICE } from '@mgremy/core';
-import { MgnpButton } from '@mgremy/ng-primitives/button';
 
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { heroBars4, heroMoon, heroSun } from '@ng-icons/heroicons/outline';
 
 import { Component, inject, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { NgpButton } from 'ng-primitives/button';
 
 @Component({
-  imports: [RouterLink, NgIcon, RouterOutlet, SidebarComponent, MgnpButton, NgpButton],
+  imports: [RouterLink, NgIcon, RouterOutlet, SidebarComponent],
   standalone: true,
   template: `
     <header class="fixed top-0 w-full z-100">
-      <nav class="flex max-w-screen justify-between p-4 items-center bg-secondary">
+      <nav class="nav justify-between">
         <div class="flex gap-2 items-center">
           <ng-icon
             name="heroBars4"
@@ -24,13 +22,14 @@ import { NgpButton } from 'ng-primitives/button';
             class="cursor-pointer"
             [routerLink]="['/']">
             <span class="sr-only">Brand</span>
-            <span class="text-xl md:text-2xl font-semibold whitespace-nowrap">&#64;mgremy/ng-primitives</span>
+            <span class="text-xl md:text-2xl font-semibold whitespace-nowrap">
+              &#64;mgremy/ng-primitives
+            </span>
           </a>
         </div>
         <div class="flex gap-4 items-center">
           <button
-            ngpButton
-            mgnpButton
+            class="nav-item"
             (click)="
               _themeService.setTheme(_themeService.getTheme() === 'light' ? 'dark' : 'light')
             ">
@@ -41,9 +40,16 @@ import { NgpButton } from 'ng-primitives/button';
               name="heroMoon"
               class="hidden! dark:inline-block!" />
           </button>
+          <a
+            href="https://github.com/MGREMY/nx_source"
+            target="_blank"
+            class="nav-item hidden sm:block">
+            Github
+          </a>
         </div>
       </nav>
-      <div class="bg-(--mg-border-emphasis) absolute inset-x-0 h-0.5"></div>
+      <div
+        class="transition-colors duration-200 ease-in-out bg-(--mg-border-emphasis) absolute inset-x-0 h-0.5"></div>
     </header>
 
     <main class="overflow-hidden mt-16 p-8">
--- apps/ng-primitives-docs/src/app/pages/index.page.ts
+++ apps/ng-primitives-docs/src/app/pages/index.page.ts
@@ -2,42 +2,86 @@ import { APP_THEME_SERVICE } from '@mgremy/core';
 import { MgnpButton } from '@mgremy/ng-primitives/button';
 
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { flagCp, flagUs } from '@ng-icons/flag-icons';
-import { heroBars4, heroMoon, heroSun } from '@ng-icons/heroicons/outline';
+import { heroMoon, heroSun } from '@ng-icons/heroicons/outline';
 
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NgpButton } from 'ng-primitives/button';
 
 @Component({
-  imports: [NgIcon, NgpButton, MgnpButton],
+  imports: [NgpButton, MgnpButton, NgIcon, RouterLink],
   standalone: true,
   template: `
     <header class="fixed top-0 w-full z-100">
-      <nav class="flex max-w-screen justify-end p-4 items-center bg-secondary/75">
-        <div class="flex gap-4 items-center">
+      <nav class="nav">
+        <div class="flex flex-1 justify-between md:max-w-5xl md:justify-end md:mx-auto">
+          <div class="flex gap-4 items-center">
+            <button
+              class="nav-item"
+              (click)="
+                _themeService.setTheme(_themeService.getTheme() === 'light' ? 'dark' : 'light')
+              ">
+              <ng-icon
+                name="heroSun"
+                class="dark:hidden! inline-block!" />
+              <ng-icon
+                name="heroMoon"
+                class="hidden! dark:inline-block!" />
+            </button>
+          </div>
+          <div class="flex">
+            <button
+              class="nav-item"
+              [routerLink]="['/', 'getting-started', 'introduction']">
+              Documentation
+            </button>
+            <a
+              href="https://github.com/MGREMY/nx_source"
+              target="_blank"
+              class="nav-item">
+              Github
+            </a>
+          </div>
+        </div>
+      </nav>
+      <div
+        class="transition-colors duration-200 ease-in-out bg-(--mg-border-emphasis) absolute inset-x-0 h-0.5"></div>
+    </header>
+
+    <main class="overflow-hidden mt-16 p-4 md:max-w-6xl md:mx-auto">
+      <section class="flex flex-col pt-16 space-y-16 items-center">
+        <div class="space-y-2">
+          <h1 class="font-bold text-2xl sm:text-5xl text-blue">@mgremy/ng-primitives</h1>
+          <p class="px-4 font-semibold text-lg sm:text-3xl">Styled wrappers for ng-primitives</p>
+        </div>
+        <div class="flex flex-col sm:flex-row items-center gap-16">
           <button
             ngpButton
             mgnpButton
-            (click)="
-              _themeService.setTheme(_themeService.getTheme() === 'light' ? 'dark' : 'light')
-            ">
-            <ng-icon
-              name="heroSun"
-              class="dark:hidden! inline-block!" />
-            <ng-icon
-              name="heroMoon"
-              class="hidden! dark:inline-block!" />
+            [routerLink]="['/', 'getting-started', 'introduction']">
+            Get Started
           </button>
+          <div class="flex gap-4">
+            <a
+              ngpButton
+              mgnpButton
+              href="https://github.com/MGREMY/nx_source"
+              target="_blank">
+              Github
+            </a>
+            <a
+              ngpButton
+              mgnpButton
+              href="https://www.npmjs.com/package/@mgremy/ng-primitives"
+              target="_blank">
+              NpmJS
+            </a>
+          </div>
         </div>
-      </nav>
-      <div class="bg-(--mg-border-emphasis) absolute inset-x-0 h-0.5"></div>
-    </header>
-
-    <main class="overflow-hidden mt-16 p-4">
-      <h1>Index</h1>
+      </section>
     </main>
   `,
-  providers: [provideIcons({ flagCp, flagUs, heroBars4, heroSun, heroMoon })],
+  providers: [provideIcons({ heroSun, heroMoon })],
 })
 export default class IndexPage {
   protected readonly _themeService = inject(APP_THEME_SERVICE);
