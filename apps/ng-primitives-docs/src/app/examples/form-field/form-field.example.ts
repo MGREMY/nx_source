731b81d8b710a73bd570657929de79f4bf5dbcb4
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-05-20 23:07:35 +0200

    feat(checkbox): add form support

--- apps/ng-primitives-docs/src/app/examples/form-field/form-field.example.ts
+++ apps/ng-primitives-docs/src/app/examples/form-field/form-field.example.ts
@@ -1,26 +1,40 @@
+import { MgnpCheckbox } from '@mgremy/ng-primitives/checkbox';
 import { MgnpDescription, MgnpError, MgnpFormField, MgnpLabel } from '@mgremy/ng-primitives/form-field';
 import { MgnpInput } from '@mgremy/ng-primitives/input';
 
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroCheckMini } from '@ng-icons/heroicons/mini';
+
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NgpInput } from 'ng-primitives/input';
 
 @Component({
-  imports: [MgnpFormField, MgnpLabel, MgnpDescription, MgnpError, MgnpInput, NgpInput, ReactiveFormsModule],
+  imports: [MgnpFormField, MgnpLabel, MgnpDescription, MgnpError, MgnpInput, MgnpCheckbox, NgIcon, ReactiveFormsModule],
   template: `
     <form class="grid grid-cols-1 md:grid-cols-2 gap-2" [formGroup]="formGroup">
       <div mgnpFormField>
         <label mgnpLabel for="email">Email</label>
         <p mgnpDescription>Enter your email in order to make this work</p>
-        <input ngpInput mgnpInput id="email" placeholder="email@domain.com" [formControl]="formGroup.controls.email" />
+        <input mgnpInput id="email" placeholder="email@domain.com" [formControl]="formGroup.controls.email" />
         <p mgnpError validator="required">This field is required.</p>
         <p mgnpError validator="email">This field must be an email.</p>
       </div>
+      <div mgnpFormField>
+        <label mgnpLabel for="email">Conditions</label>
+        <span mgnpCheckbox [formControl]="formGroup.controls.checked">
+          @if (formGroup.controls.checked.value === true) {
+            <ng-icon name="heroCheckMini" />
+          }
+        </span>
+        <p mgnpError validator="required">Please accept the conditions.</p>
+      </div>
     </form>
   `,
+  providers: [provideIcons({ heroCheckMini })],
 })
 export default class FormFieldExample {
   readonly formGroup = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
+    checked: new FormControl(false, [Validators.requiredTrue]),
   });
 }
--- apps/ng-primitives-docs/src/app/examples/form-field/signal-form.example.ts
+++ apps/ng-primitives-docs/src/app/examples/form-field/signal-form.example.ts
@@ -1,29 +1,46 @@
+import { MgnpCheckbox } from '@mgremy/ng-primitives/checkbox';
 import { MgnpDescription, MgnpError, MgnpFormField, MgnpLabel } from '@mgremy/ng-primitives/form-field';
 import { MgnpInput } from '@mgremy/ng-primitives/input';
 
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroCheckMini } from '@ng-icons/heroicons/mini';
+
 import { Component, signal } from '@angular/core';
-import { email, form, FormField, required } from '@angular/forms/signals';
-import { NgpInput } from 'ng-primitives/input';
+import { email, form, FormField, FormRoot, required } from '@angular/forms/signals';
 
 @Component({
-  imports: [MgnpFormField, MgnpLabel, MgnpDescription, MgnpError, MgnpInput, NgpInput, FormField],
+  imports: [MgnpFormField, MgnpLabel, MgnpDescription, MgnpError, MgnpInput, MgnpCheckbox, NgIcon, FormField, FormRoot],
   template: `
-    <div mgnpFormField>
-      <label mgnpLabel for="email">Email</label>
-      <p mgnpDescription>Enter your email in order to make this work</p>
-      <input ngpInput mgnpInput id="email" placeholder="email@domain.com" [formField]="form.email" />
-      <p mgnpError validator="required">This field is required</p>
-      <p mgnpError validator="email">This field must be an email</p>
-    </div>
+    <form class="grid grid-cols-1 md:grid-cols-2 gap-2" [formRoot]="form">
+      <div mgnpFormField>
+        <label mgnpLabel for="email">Email</label>
+        <p mgnpDescription>Enter your email in order to make this work</p>
+        <input mgnpInput id="email" placeholder="email@domain.com" [formField]="form.email" />
+        <p mgnpError validator="required">This field is required</p>
+        <p mgnpError validator="email">This field must be an email</p>
+      </div>
+      <div mgnpFormField>
+        <label mgnpLabel for="email">Conditions</label>
+        <span mgnpCheckbox [formField]="form.checked">
+          @if (form.checked().value() === true) {
+            <ng-icon name="heroCheckMini" />
+          }
+        </span>
+        <p mgnpError validator="required">Please accept the conditions.</p>
+      </div>
+    </form>
   `,
+  providers: [provideIcons({ heroCheckMini })],
 })
 export default class SignalFormExample {
   private readonly formModel = signal({
     email: '',
+    checked: false,
   });
 
   readonly form = form(this.formModel, (root) => {
     required(root.email);
     email(root.email);
+    required(root.checked);
   });
 }
