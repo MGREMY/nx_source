126cabdb0fd58b5217deeefbdae32c25c9edbc58
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-05-20 21:55:08 +0200

    feat(pagination): migrate to hostDirective

--- apps/ng-primitives-docs/src/app/examples/pagination/pagination.example.ts
+++ apps/ng-primitives-docs/src/app/examples/pagination/pagination.example.ts
@@ -1,4 +1,11 @@
-import { MgnpPagination } from '@mgremy/ng-primitives/pagination';
+import {
+  MgnpPagination,
+  MgnpPaginationButton,
+  MgnpPaginationFirst,
+  MgnpPaginationLast,
+  MgnpPaginationNext,
+  MgnpPaginationPrevious,
+} from '@mgremy/ng-primitives/pagination';
 
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import {
@@ -9,58 +16,44 @@ import {
 } from '@ng-icons/heroicons/outline';
 
 import { Component, signal } from '@angular/core';
-import {
-  NgpPagination,
-  NgpPaginationButton,
-  NgpPaginationFirst,
-  NgpPaginationLast,
-  NgpPaginationNext,
-  NgpPaginationPrevious,
-} from 'ng-primitives/pagination';
 
 @Component({
   imports: [
-    NgpPagination,
-    NgpPaginationButton,
-    NgpPaginationFirst,
-    NgpPaginationPrevious,
-    NgpPaginationNext,
-    NgpPaginationLast,
     MgnpPagination,
+    MgnpPaginationButton,
+    MgnpPaginationFirst,
+    MgnpPaginationPrevious,
+    MgnpPaginationNext,
+    MgnpPaginationLast,
     NgIcon,
   ],
   template: `
-    <nav
-      [(ngpPaginationPage)]="page"
-      ngpPagination
-      ngpPaginationPageCount="5"
-      mgnpPagination
-      aria-label="Pagination Navigation">
+    <nav mgnpPagination [(page)]="page" pageCount="5" aria-label="Pagination Navigation">
       <ul>
         <li>
-          <a ngpPaginationFirst aria-label="First Page">
+          <a mgnpPaginationFirst aria-label="First Page">
             <ng-icon name="heroChevronDoubleLeft" />
           </a>
         </li>
         <li>
-          <a ngpPaginationPrevious aria-label="Previous Page">
+          <a mgnpPaginationPrevious aria-label="Previous Page">
             <ng-icon name="heroChevronLeft" />
           </a>
         </li>
 
-        <li><a ngpPaginationButton ngpPaginationButtonPage="1" aria-label="Page 1">1</a></li>
-        <li><a ngpPaginationButton ngpPaginationButtonPage="2" aria-label="Page 2">2</a></li>
-        <li><a ngpPaginationButton ngpPaginationButtonPage="3" aria-label="Page 3">3</a></li>
-        <li><a ngpPaginationButton ngpPaginationButtonPage="4" aria-label="Page 4">4</a></li>
-        <li><a ngpPaginationButton ngpPaginationButtonPage="5" aria-label="Page 5">5</a></li>
+        <li><a mgnpPaginationButton page="1" aria-label="Page 1">1</a></li>
+        <li><a mgnpPaginationButton page="2" aria-label="Page 2">2</a></li>
+        <li><a mgnpPaginationButton page="3" aria-label="Page 3">3</a></li>
+        <li><a mgnpPaginationButton page="4" aria-label="Page 4">4</a></li>
+        <li><a mgnpPaginationButton page="5" aria-label="Page 5">5</a></li>
 
         <li>
-          <a ngpPaginationNext aria-label="Next Page">
+          <a mgnpPaginationNext aria-label="Next Page">
             <ng-icon name="heroChevronRight" />
           </a>
         </li>
         <li>
-          <a ngpPaginationLast aria-label="Last Page">
+          <a mgnpPaginationLast aria-label="Last Page">
             <ng-icon name="heroChevronDoubleRight" />
           </a>
         </li>
--- libs/ng-primitives/pagination/src/index.ts
+++ libs/ng-primitives/pagination/src/index.ts
@@ -1,1 +1,6 @@
 export * from './lib/mgnp-pagination/mgnp-pagination.directive';
+export * from './lib/mgnp-pagination-button/mgnp-pagination-button.directive';
+export * from './lib/mgnp-pagination-first/mgnp-pagination-first.directive';
+export * from './lib/mgnp-pagination-last/mgnp-pagination-last.directive';
+export * from './lib/mgnp-pagination-next/mgnp-pagination-next.directive';
+export * from './lib/mgnp-pagination-previous/mgnp-pagination-previous.directive';
