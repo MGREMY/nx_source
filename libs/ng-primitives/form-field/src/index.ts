c49ced317374d8dd607c70791944410f8295b9dc
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-06-15 23:46:19 +0200

    fix(form-field): remove export from index.ts

--- libs/ng-primitives/form-field/src/index.ts
+++ libs/ng-primitives/form-field/src/index.ts
@@ -2,6 +2,5 @@ export * from './lib/mgnp-description/mgnp-description.directive';
 export * from './lib/mgnp-error/mgnp-error.directive';
 export * from './lib/mgnp-form-control/mgnp-form-control.directive';
 export * from './lib/mgnp-form-field/mgnp-form-field.directive';
-export * from './lib/mgnp-icon-group/mgnp-icon-group.directive';
 export * from './lib/mgnp-label/mgnp-form-label.directive';
 export * from './lib/mgnp-label-group/mgnp-label-group.directive';
