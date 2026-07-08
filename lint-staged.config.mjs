2e36f53f9f71f52f8434a646be2f475fa4d2859d
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-07-08 09:38:14 +0200

    chore: specify files for format:write

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,7 +1,7 @@
 export default {
   '*': (files) => {
     return [
-      'pnpm exec nx format:write',
+      `pnpm exec nx format:write --files=${files.join(',')}`,
       `pnpm exec nx affected --target=lint --files=${files.join(',')}`,
     ];
   },
