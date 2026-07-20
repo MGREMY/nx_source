bfb7f9288291663a13ad265e83eb263c242b1f2f
GREMY Miguel <miguel.gremy72+pro@gmail.com>
2026-07-20 11:02:08 +0200

    refactor: update table style

--- packages/ng-primitives-extended/table/src/lib/mgnp-table-footer/mgnp-table-footer.ts
+++ packages/ng-primitives-extended/table/src/lib/mgnp-table-footer/mgnp-table-footer.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MgnpTable } from '../mgnp-table/mgnp-table';
+
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 @Component({
   selector: 'mgnp-table-footer, tr[mgnpTableFooter]',
@@ -9,6 +11,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   host: {
     'data-mgnp-table-footer': '',
     class: 'mgnp-table-footer mgnp-c-table-footer',
+    '[attr.data-mgnp-table-footer-color]': 'table.color()',
   },
 })
-export class MgnpTableFooter {}
+export class MgnpTableFooter {
+  protected readonly table = inject(MgnpTable);
+}
