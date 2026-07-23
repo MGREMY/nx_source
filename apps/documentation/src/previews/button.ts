7489a06673275600f4712e0b6d7a035cb1cea5fe
Miguel Gremy <miguel.gremy72+pro@gmail.com>
2026-07-23 23:15:10 +0200

    docs: improve color display by looping around (#42)

--- apps/documentation/src/previews/button.ts
+++ apps/documentation/src/previews/button.ts
@@ -7,37 +7,27 @@ import { Component } from '@angular/core';
   template: `
     <p class="font-bold">Solid</p>
     <div class="flex flex-wrap gap-4 items-center justify-center">
-      <button mgnpButton>Default</button>
-      <button mgnpButton color="primary">Primary</button>
-      <button mgnpButton color="accent">Accent</button>
-      <button mgnpButton color="info">Info</button>
-      <button mgnpButton color="success">Success</button>
-      <button mgnpButton color="warning">Warning</button>
-      <button mgnpButton color="danger">Danger</button>
+      @for (color of _colors; track $index) {
+        <button mgnpButton [color]="color">{{ color }}</button>
+      }
     </div>
     <p class="font-bold">Outline</p>
     <div class="flex flex-wrap gap-4 items-center justify-center">
-      <button mgnpButton variant="outline">Default</button>
-      <button mgnpButton variant="outline" color="primary">Primary</button>
-      <button mgnpButton variant="outline" color="accent">Accent</button>
-      <button mgnpButton variant="outline" color="info">Info</button>
-      <button mgnpButton variant="outline" color="success">Success</button>
-      <button mgnpButton variant="outline" color="warning">Warning</button>
-      <button mgnpButton variant="outline" color="danger">Danger</button>
+      @for (color of _colors; track $index) {
+        <button mgnpButton variant="outline" [color]="color">{{ color }}</button>
+      }
     </div>
     <p class="font-bold">Disabled</p>
     <div class="flex flex-wrap gap-4 items-center justify-center">
-      <button mgnpButton disabled>Default</button>
-      <button mgnpButton disabled color="primary">Primary</button>
-      <button mgnpButton disabled color="accent">Accent</button>
-      <button mgnpButton disabled color="info">Info</button>
-      <button mgnpButton disabled color="success">Success</button>
-      <button mgnpButton disabled color="warning">Warning</button>
-      <button mgnpButton disabled color="danger">Danger</button>
+      @for (color of _colors; track $index) {
+        <button mgnpButton disabled variant="outline" [color]="color">{{ color }}</button>
+      }
     </div>
   `,
   host: {
     class: 'flex! flex-col flex-wrap gap-4 items-center',
   },
 })
-export default class Button {}
+export default class Button {
+  readonly _colors = ['ui', 'primary', 'accent', 'info', 'success', 'warning', 'danger'];
+}
--- apps/documentation/src/previews/checkbox.ts
+++ apps/documentation/src/previews/checkbox.ts
@@ -5,28 +5,19 @@ import { Component } from '@angular/core';
 @Component({
   imports: [MgnpCheckbox],
   template: `
-    <div class="flex flex-wrap gap-4 items-center">
-      <span mgnpCheckbox></span>
-      <span mgnpCheckbox color="primary"></span>
-      <span mgnpCheckbox color="accent"></span>
-      <span mgnpCheckbox color="info"></span>
-      <span mgnpCheckbox color="success"></span>
-      <span mgnpCheckbox color="warning"></span>
-      <span mgnpCheckbox color="danger"></span>
-    </div>
-    <p class="font-bold">Indeterminate</p>
-    <div class="flex flex-wrap gap-4 items-center">
-      <span mgnpCheckbox mgnpCheckboxIndeterminate></span>
-      <span mgnpCheckbox mgnpCheckboxIndeterminate color="primary"></span>
-      <span mgnpCheckbox mgnpCheckboxIndeterminate color="accent"></span>
-      <span mgnpCheckbox mgnpCheckboxIndeterminate color="info"></span>
-      <span mgnpCheckbox mgnpCheckboxIndeterminate color="success"></span>
-      <span mgnpCheckbox mgnpCheckboxIndeterminate color="warning"></span>
-      <span mgnpCheckbox mgnpCheckboxIndeterminate color="danger"></span>
+    <div
+      class="grid grid-cols-[max-content_minmax(max-content,1fr)_minmax(max-content,1fr)] gap-x-4 gap-y-2 items-center w-full">
+      <span></span>
+      <span></span>
+      <span class="justify-self-center">Indeterminate</span>
+      @for (color of _colors; track $index) {
+        <span>{{ color }}</span>
+        <span class="justify-self-center" mgnpCheckbox [color]="color"></span>
+        <span class="justify-self-center" mgnpCheckbox mgnpCheckboxIndeterminate [color]="color"></span>
+      }
     </div>
   `,
-  host: {
-    class: 'flex! flex-col flex-wrap gap-4 items-center',
-  },
 })
-export default class Checkbox {}
+export default class Checkbox {
+  readonly _colors = ['ui', 'primary', 'accent', 'info', 'success', 'warning', 'danger'];
+}
