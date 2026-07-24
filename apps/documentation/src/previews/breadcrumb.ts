3d63d84d456ba149a22d7be3de4a468a31bd0264
Miguel Gremy <miguel.gremy72+pro@gmail.com>
2026-07-24 10:05:45 +0200

    feat(breadcrumb): add colors (#43)

--- apps/documentation/src/previews/breadcrumb.ts
+++ apps/documentation/src/previews/breadcrumb.ts
@@ -31,52 +31,59 @@ import { RouterLink } from '@angular/router';
     RouterLink,
   ],
   template: `
-    <nav aria-label="Breadcrumb" mgnpBreadcrumb>
-      <ol mgnpBreadcrumbList>
-        <li mgnpBreadcrumbItem>
-          <a mgnpBreadcrumbLink [routerLink]="['/']">Home</a>
-        </li>
+    <div class="grid grid-cols-[max-content_1fr] gap-x-4 gap-y-2 w-full items-center">
+      @for (color of _colors; track $index) {
+        <span>{{ color }}</span>
+        <nav class="justify-self-center" aria-label="Breadcrumb" mgnpBreadcrumb [color]="color">
+          <ol mgnpBreadcrumbList>
+            <li mgnpBreadcrumbItem>
+              <a mgnpBreadcrumbLink [routerLink]="['/']">Home</a>
+            </li>
 
-        <li mgnpBreadcrumbSeparator aria-hidden="true">
-          <ng-icon name="heroChevronRight" />
-        </li>
+            <li mgnpBreadcrumbSeparator aria-hidden="true">
+              <ng-icon name="heroChevronRight" />
+            </li>
 
-        <li mgnpBreadcrumbItem>
-          <button [mgnpMenuTrigger]="breadcrumbMenu" type="button" aria-label="Toggle breadcrumb menu">
-            <span mgnpBreadcrumbEllipsis>
-              <ng-icon name="heroEllipsisHorizontal" />
-            </span>
-          </button>
+            <li mgnpBreadcrumbItem>
+              <button [mgnpMenuTrigger]="breadcrumbMenu" type="button" aria-label="Toggle breadcrumb menu">
+                <span mgnpBreadcrumbEllipsis>
+                  <ng-icon name="heroEllipsisHorizontal" />
+                </span>
+              </button>
 
-          <ng-template #breadcrumbMenu>
-            <div mgnpMenu>
-              <button mgnpMenuItem>Some</button>
-              <button mgnpMenuItem>Other</button>
-              <button mgnpMenuItem>Options</button>
-              <button mgnpMenuItem>Or</button>
-              <button mgnpMenuItem>Actions</button>
-            </div>
-          </ng-template>
-        </li>
+              <ng-template #breadcrumbMenu>
+                <div mgnpMenu [color]="color">
+                  <button mgnpMenuItem>Some</button>
+                  <button mgnpMenuItem>Other</button>
+                  <button mgnpMenuItem>Options</button>
+                  <button mgnpMenuItem>Or</button>
+                  <button mgnpMenuItem>Actions</button>
+                </div>
+              </ng-template>
+            </li>
 
-        <li mgnpBreadcrumbSeparator aria-hidden="true">
-          <ng-icon name="heroChevronRight" />
-        </li>
+            <li mgnpBreadcrumbSeparator aria-hidden="true">
+              <ng-icon name="heroChevronRight" />
+            </li>
 
-        <li mgnpBreadcrumbItem>
-          <span>Components</span>
-        </li>
+            <li mgnpBreadcrumbItem>
+              <span>Components</span>
+            </li>
 
-        <li mgnpBreadcrumbSeparator aria-hidden="true">
-          <ng-icon name="heroChevronRight" />
-        </li>
+            <li mgnpBreadcrumbSeparator aria-hidden="true">
+              <ng-icon name="heroChevronRight" />
+            </li>
 
-        <li mgnpBreadcrumbItem>
-          <a mgnpBreadcrumbPage [routerLink]="['/', 'components', 'breadcrumb']">Breadcrumb</a>
-        </li>
-      </ol>
-    </nav>
+            <li mgnpBreadcrumbItem>
+              <a mgnpBreadcrumbPage [routerLink]="['/', 'components', 'breadcrumb']">Breadcrumb</a>
+            </li>
+          </ol>
+        </nav>
+      }
+    </div>
   `,
   providers: [provideIcons({ heroChevronRight, heroEllipsisHorizontal })],
 })
-export default class Breadcrumb {}
+export default class Breadcrumb {
+  readonly _colors = ['ui', 'primary', 'accent', 'info', 'success', 'warning', 'danger'];
+}
