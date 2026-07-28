7c5d7760f0b8e47efe47677f8f547cf0fdbc7046
Miguel Gremy <miguel.gremy72+pro@gmail.com>
2026-07-28 10:55:10 +0200

    feat(file-upload): add file-upload (#51)
    * feat(file-upload): add file-upload
    
    * refactor: coderabbit comments

---
+++ apps/documentation/src/content/documentation/ng-primitives/file-upload.md
@@ -0,0 +1,24 @@
+---
+name: 'File Upload'
+sourceUrl: 'https://github.com/mgremy/nx_source/tree/main/packages/ng-primitives/file-upload'
+primitiveUrl: 'https://angularprimitives.com/primitives/file-upload'
+---
+
+# File Upload
+
+The file upload primitive allows you to trigger a file upload from any element, giving you more
+control over the appearance and behavior compared to the native file input.
+
+## Usage
+
+<app-preview name="file-upload"></app-preview>
+
+<app-examples name="file-upload"></app-examples>
+
+## Theme
+
+<app-css-content name="file-upload"></app-css-content>
+
+## Metadata
+
+<app-metadata name="file-upload"></app-metadata>
---
+++ packages/ng-primitives/file-upload/src/lib/mgnp-file-upload/mgnp-file-upload.ts
@@ -0,0 +1,40 @@
+import { PropertyType } from '@mgremy/ng-primitives';
+
+import { Directive, input } from '@angular/core';
+import { NgpFileUpload, provideFileUploadState } from 'ng-primitives/file-upload';
+
+export type MgnpFileUploadColor = PropertyType<
+  'ui' | 'primary' | 'accent' | 'info' | 'success' | 'warning' | 'danger'
+>;
+
+@Directive({
+  selector: '[mgnpFileUpload]',
+  providers: [provideFileUploadState()],
+  host: {
+    class: 'mgnp-file-upload mgnp-c-file-upload',
+    'data-mgnp-file-upload': '',
+    '[attr.data-mgnp-file-upload-color]': 'color()',
+  },
+  hostDirectives: [
+    {
+      directive: NgpFileUpload,
+      inputs: [
+        'ngpFileUploadFileTypes:mgnpFileUploadFileTypes',
+        'ngpFileUploadMultiple:mgnpFileUploadMultiple',
+        'ngpFileUploadDirectory:mgnpFileUploadDirectory',
+        'ngpFileUploadDragDrop:mgnpFileUploadDragDrop',
+        'ngpFileUploadDisabled:mgnpFileUploadDisabled',
+      ],
+      outputs: [
+        'ngpFileUploadSelected:mgnpFileUploadSelected',
+        'ngpFileUploadCanceled:mgnpFileUploadCanceled',
+        'ngpFileUploadRejected:mgnpFileUploadRejected',
+        'ngpFileUploadDragOver:mgnpFileUploadDragOver',
+      ],
+    },
+  ],
+  exportAs: 'mgnpFileUpload',
+})
+export class MgnpFileUpload {
+  readonly color = input<MgnpFileUploadColor>('ui');
+}
